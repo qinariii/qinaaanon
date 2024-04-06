6f628570609bbeda95d35ca8d745b64dd8c8058f
qinaaprojext <140184410+qinariii@users.noreply.github.com>
2024-04-06 20:57:53 +0700

    tes

--- index.js
+++ index.js
@@ -2,5 +2,18 @@ const { makeWASocket, useMultiFileAuthState, } = require('@whiskeysockets/bailey
 const pino = require('pino')
 
 ansync function connectWhatsapp() {
-  
-jk
+    const auth = await useMultiFileAuthState("sessions");
+    const socket = makeWASocket({
+        printQRInTerminal: true,
+        browser: [ "QinaaBot","","" ],
+        auth: auth.state,
+        logger: pino({ level: "silent"}),
+    });
+
+    socket.ev.on("creds.update", auth.saveCreds);
+    socket.ev.on("connection.update", async ({ connection }) => {
+        if (connection === "open") {
+            console.log(" BOT READY");
+        } else if (connection === "close") {
+            await connectWhatsapp();
+    )}
