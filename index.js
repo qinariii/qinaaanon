4281247b2f9c85013cda516f9417f6cde258df49
qinaaprojext <140184410+qinariii@users.noreply.github.com>
2024-04-06 21:05:59 +0700

    es

--- index.js
+++ index.js
@@ -16,4 +16,14 @@ ansync function connectWhatsapp() {
             console.log(" BOT READY");
         } else if (connection === "close") {
             await connectWhatsapp();
-    )}
+    });
+
+    socket.ev.on("messages.upsert", async ({ messages,type }) => {
+        const chat = messages[0]
+        const pesan = ( chat.message?.extendedTextMessage?.text ?? chat.message?.emphemeralMessage?.message?.extendedTextMessage?.txt ??chat.message?.conversation)?toLowerCase() || "";
+
+        if (pesan == '.ping') {
+            await socket.sendMessage(chat.key.remotedJid { text: "Hello."}, { quoted: chat})
+            await socket.sendMessage(chat.key.remotedJid { text: "Hello."}, { quoted: chat})
+        }
+connectWhatsapp()
